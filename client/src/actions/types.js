46052d70ded960024dda9031348dd54a8ef10863
HarshVikram <harsh.vikram34@gmail.com>
2021-05-02 13:24:37 +0530

    reducer and action created for Product

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,7 @@ export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const GET_PRODUCT = 'GET_PRODUCT';
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+export const PRODUCT_ERROR = 'PRODUCT_ERROR'
